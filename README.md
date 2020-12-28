57b05b4a0d9a5af727e52c3ca69b0b23f675f4a1
arvinxx <arvinx@foxmail.com>
2020-12-28 14:41:25 +0800

    :sparkles: feat: 完成脚手架制作

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 基于 Umi 的 Chrome 插件开发脚手架
 
-
+使用插件 `umi-plugin-extension`
--- config/config.ts
+++ config/config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from 'umi';
-import Config from 'webpack-chain';
-import GenerateJsonPlugin from 'generate-json-webpack-plugin';
-
-import manifest from '../src/manifest';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-
-  chainWebpack: (config: Config) => {
-    config.entry('background').add('@/background/index');
-
-    config
-      .plugin('toJSON')
-      .use(GenerateJsonPlugin, ['manifest.json', manifest]);
-  },
   locale: { default: 'zh-CN' },
-  routes: [
-    { path: '/popup', component: '@/popup/index' },
-    { path: '/options', component: '@/options/index' },
-  ],
-  mpa: {},
+  extensions: {
+    name: 'Umi Chrome Extension Template',
+    description: '基于 Umi 的 Chrome 插件开发脚手架',
+    optionsUI: {
+      page: '@/pages/options',
+      openInTab: true,
+    },
+    background: { scripts: ['@/background/index'] },
+    popupUI: '@/pages/popup',
+    icons: {
+      16: 'logo/logo@16.png',
+      32: 'logo/logo@32.png',
+      48: 'logo/logo@48.png',
+      128: 'logo/logo@128.png',
+    },
+  },
 });
